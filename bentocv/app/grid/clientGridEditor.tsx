1f695bb2efd3e75c30385639cf03ee886832e6f4
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2026-06-19 12:33:17 +0200

    Update clientGridEditor.tsx

--- bentocv/app/grid/clientGridEditor.tsx
+++ bentocv/app/grid/clientGridEditor.tsx
@@ -15,6 +15,20 @@ interface clientGridEditorProps {
 export default function clientGridEditor({ initialProfile }: clientGridEditorProps) {
     const [profile] = useState(initialProfile);
     const [widgetsToAdd, setWidgetsToAdd] = useState<string[]>([]);
+    const [widgetsStatus, setWidgetsStatus] = useState<{ [key: string]: boolean }>({
+        age: false,
+        dateOfBirth: false,
+        email: false,
+        phoneNumber: false,
+        socialNetwork: false,
+        techStack: false,
+        project: false,
+        motto: false,
+        softSkill: false,
+        hardSkill: false,
+        driverLicense: false,
+        languages: false,
+    });
     const router = useRouter();
     const supabase = createClient();
 
@@ -31,8 +45,26 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
 
 
     const handleAddWidget = (widgetName: string) => {
-        setWidgetsToAdd([...widgetsToAdd, widgetName]);
-    };
+    const nextState = !widgetsStatus[widgetName];
+    
+    setWidgetsStatus(prev => ({
+        ...prev,
+        [widgetName]: nextState
+    }));
+
+    if (nextState) {
+        setWidgetsToAdd((prevWidgets) => {
+            if (!prevWidgets.includes(widgetName)) {
+                return [...prevWidgets, widgetName];
+            }
+            return prevWidgets;
+        });
+    } else {
+        setWidgetsToAdd((prevWidgets) => 
+            prevWidgets.filter((name) => name !== widgetName)
+        );
+    }
+};
 
     return (
         <div className="min-h-screen bg-black text-white p-8 font-sans selection:bg-zinc-800">
@@ -70,7 +102,10 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Età</p>
                         </div>
-                        <i onClick={() => handleAddWidget('age')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 0)}
+                            className={`fa-solid ${!widgetsStatus[0] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -80,7 +115,10 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Data di nascita</p>
                         </div>
-                        <i onClick={() => handleAddWidget('dateOfBirth')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 1)}
+                            className={`fa-solid ${!widgetsStatus[1] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -90,7 +128,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">E-mail di contatto</p>
                         </div>
-                        <i onClick={() => handleAddWidget('email')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 2)}
+                            className={`fa-solid ${!widgetsStatus[2] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -100,7 +142,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Numero di telefono</p>
                         </div>
-                        <i onClick={() => handleAddWidget('phoneNumber')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 3)}
+                            className={`fa-solid ${!widgetsStatus[3] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -110,7 +156,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Social Network</p>
                         </div>
-                        <i onClick={() => handleAddWidget('socialNetwork')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 4)}
+                            className={`fa-solid ${!widgetsStatus[4] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -120,7 +170,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Tech Stack preferita</p>
                         </div>
-                        <i onClick={() => handleAddWidget('techStack')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 5)}
+                            className={`fa-solid ${!widgetsStatus[5] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -130,7 +184,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Progetto Vetrina</p>
                         </div>
-                        <i onClick={() => handleAddWidget('project')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 6)}
+                            className={`fa-solid ${!widgetsStatus[6] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -140,7 +198,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Motto di vita</p>
                         </div>
-                        <i onClick={() => handleAddWidget('motto')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 7)}
+                            className={`fa-solid ${!widgetsStatus[7] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -150,7 +212,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Soft Skill</p>
                         </div>
-                        <i onClick={() => handleAddWidget('softSkill')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 8)}
+                            className={`fa-solid ${!widgetsStatus[8] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -160,7 +226,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Hard Skill</p>
                         </div>
-                        <i onClick={() => handleAddWidget('hardSkill')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 9)}
+                            className={`fa-solid ${!widgetsStatus[9] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -170,7 +240,11 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Patente di guida</p>
                         </div>
-                        <i onClick={() => handleAddWidget('driverLicense')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 10)}
+                            className={`fa-solid ${!widgetsStatus[10] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
+
                     </div>
 
                     <div className="p-3.5 bg-zinc-900/60 border border-zinc-800/60 hover:border-zinc-700 hover:bg-zinc-900 rounded-xl cursor-pointer transition-all flex items-center justify-between group active:scale-[0.99]">
@@ -180,7 +254,10 @@ export default function clientGridEditor({ initialProfile }: clientGridEditorPro
                             </div>
                             <p className="text-xs font-medium text-zinc-400 group-hover:text-zinc-200 transition-colors">Lingue conosciute</p>
                         </div>
-                        <i onClick={() => handleAddWidget('languages')} className="fa-solid fa-plus text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors"></i>
+                        <i
+                            onClick={() => handleAddWidget('age', 11)}
+                            className={`fa-solid ${!widgetsStatus[11] ? 'fa-plus' : 'fa-minus'} text-[10px] text-zinc-600 group-hover:text-emerald-400 transition-colors`}
+                        ></i>
                     </div>
 
                 </aside>
